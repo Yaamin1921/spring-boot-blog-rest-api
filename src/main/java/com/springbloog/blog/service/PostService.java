d7a3c3ca2dabb52458498579242e99020086da1b
Mohd Yaamin <yaamin1921413@gmail.com>
2025-10-04 22:35:47 +0530

    feat(blog-rest-api) : add pagination and sorting for getAll posts Api

--- src/main/java/com/springbloog/blog/service/PostService.java
+++ src/main/java/com/springbloog/blog/service/PostService.java
@@ -2,12 +2,13 @@ package com.springbloog.blog.service;
 
 import com.springbloog.blog.entity.Post;
 import com.springbloog.blog.payload.PostDto;
+import com.springbloog.blog.payload.PostResponse;
 
 import java.util.List;
 
 public interface PostService {
     PostDto createPost(PostDto postDto);
-    List<PostDto> getAllPosts();
+    PostResponse getAllPosts(int pageNo, int pageSize,String sortBy,String sortDir);
     PostDto getPostById(Long id);
     PostDto updatePost(PostDto postDto,Long id);
     void deletePostById(Long id);
